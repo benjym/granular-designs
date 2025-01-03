b1a2a1b4a5b9c50974fdb3ca7fb8fb72ccdf5fb0
benjym <benjy.marks@sydney.edu.au>
2025-01-03 05:49:18 +0000

    Deploying to gh-pages from @ benjym/granular-designs@43c068b99afa71dee84db21432c01b1a65fbff37 🚀

--- apollonian_3d-bundle.js
+++ apollonian_3d-bundle.js
@@ -3055,7 +3055,7 @@ eval("module.exports = /*#__PURE__*/JSON.parse('{\"glyphs\":{\"0\":{\"x_min\":51
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
 /******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
